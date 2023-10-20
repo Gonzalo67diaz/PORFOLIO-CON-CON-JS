2f9ab3dead335c257a3faf12eef773ad5e0786f4
gonzalo diaz <128557984+Gonzalo67diaz@users.noreply.github.com>
2023-10-20 13:49:00 -0300

    prrrr

---
+++ data.js
@@ -0,0 +1,107 @@
+const proyectos = [
+
+  {
+    titulo: "PAGINA-WEB-ECOMMERCE-TIENDA-DE-TECNOLOGIA",
+    descripcion:
+      "hola esta es una pagina web completa ademas pense el diseño desde el lado del usuario visualmente siempre me gusta darle importancia al diseño para que sea una buena experecia visual. esta pagina tiene un menu con un buscador tiene un ecommerce donde tiene un carrito de compras donde podes selcionar los prodcutos se filtran ademas te lleva a otra pestaña donde te muestra tu compra con la opcion de cancelar la compra y volver a la pagina anterior es totalmente responsive.",
+    link: "https://gonzalo67diaz.github.io/PAGINA-WEB-ECOMMERCE-TIENDA-DE-TECNOLOGIA/",
+    imagen: "tecno.png",
+    tecnologias: ["HTML","CSS","Javascript"],
+  },
+
+  {
+    titulo: "PAGINA-WEB-ECOMMERCE-TIENDA-DE-ROPA",
+    descripcion:
+      "WEB RESPONSIVE ECOMMERCE TIENDA DE ROPA HOLA ESTA UNA PAGINA WEB DE ROPA DE BASQUET CUENTA CON UN MENU QUE PODES SELECIONAR LO QUE FIGURA EN EL MENU SI ELIJES LAKERS TE MUESTRA EL PRODUCTO DE BASQUET DE LAKERS ESTO SE DESLISA COMO SLIDER Y CUENTA CON UN BOTON DE COMPRA DONDE SI LO SELECCIONAS PARA COMPRAR TE REDIRECCIONA AL PRODUCTO EN ESTE CASO CAMISETAS DE BASQUET Y PUEDES SELECCIONAR TAMBIEN EL BOTON DE COMPRA Y EN PANTALLA TE MUESTRA UN FORMULARIO QUE HICE QUE DE PAGO PARA PONER LOS DATOS DE LA TARJETA PARA COMPRAR Y ADEMAS CUENTA CON UN MENU CON BUSCADOR Y TODO ES RESPONSIVE.",
+    link: "https://653006d937c52a2aefcab6ae--dynamic-klepon-ac10be.netlify.app/",
+    imagen: "tienda.png",
+    tecnologias: ["HTML","CSS","Javascript"],
+  },
+
+  {
+    titulo: "MENU-RESPONSIVE-MAS-BANNER",
+    descripcion:
+      "MENU RESPONSIVE MAS BANNER hola en este proyecto realize un menu responsive y ademas le sume un buscador que tambien es adaptable para complementar le sume un banner con una descripcion y un boton de compra implementando grid la idea es mostrar la funcionalidad y el diseño.",
+    link: "https://6530053ea659c618df8c9034--zippy-heliotrope-4fff23.netlify.app/",
+    imagen: "menu.png",
+    tecnologias: ["HTML","CSS","Javascript"],
+  },
+ 
+ { titulo: "PORFOLIO CON CON JS",
+  descripcion:
+    "hola mi nombre es Gonzalo Diaz soy de la Provincia de Cordoba Capital soy desarrollador web FULL STACK, Soy estudiante de la UTN y este es mi porfolio con mis proyectos desarrollados donde muestro mis conocimientos en el area de front y back muy pronto se vienen nuevos proyectos a subir siempre busco practicar y seguir capacitandome.",
+  link: "https://6532ad8cb638c706ff9bd43e--luxury-faun-6c9ab8.netlify.app/",
+  imagen: "porfolio.png",
+  tecnologias: ["HTML","CSS","Javascript"],
+},
+  
+  {
+    titulo: "GALERIA-DE-TIENDA-GAMER-CON-SELECTOR-DE-IMAGEN-JS",
+    descripcion:
+      "GALERIA-DE-TIENDA-GAMER-CON-SELECTOR-DE-IMAGEN-JS GALERIA DE TIENDA GAMER CON SELECTOR DE IMAGEN JS HOLA ESTE ES MI PROYECTO DE GALERIA DE IMAGEN APLICANDO GRID ADEMAS TIENE ENLACES QUE TE LLEVAN A VIDEOS DE YOUTUBE CUANDO PRECIONAS SOBRE LA IMAGEN Y CUENTA CON LA POSIBILIDAD DE AMPLIAR LA IMAGEN CUANDO HACES CLICK SOBRE LAM IMAGEN QUE ELIGAS Y ES TOTALMENTE RESPONSIVE Y LE SUME UN MENU TAMBIEN RESPONSIVE PARA SIMULAR COMO QUEDA DISEÑADA LA WEB.",
+    link: "https://653006f27bdcdc1bebae12b6--kaleidoscopic-pithivier-56e2b9.netlify.app/",
+    imagen: "galeria.png",
+    tecnologias: ["HTML", "CSS", "Javascript"],
+  },
+
+  {
+    titulo: "SLIDER-GAMER-RESPONSIVE",
+    descripcion:
+      "SLIDER-GAMER-RESPONSIVE- SLIDER JS hola este un proyecto para mostrar la funcionalidad de el slider que es responsive y ademas cuenta con un texto de descripcion que tambie es adaptable la idea",
+    link: "https://6530070d1ea15c187e095ab1--delightful-fenglisu-99beee.netlify.app/",
+    imagen: "slider.png",
+    tecnologias: ["HTML", "CSS", "Javascript"],
+  },
+
+  {
+    titulo: "PROYECTO REQUIERE INGRESAR PhpMyAdmin, PHP Y SQL TABLA DE LOGIN DE USUARIO Y REGISTRO DE ALUMNOS",
+    descripcion:
+      "back proyecto con tablas y php tiene un login que le puse de usuario gon y contraseña 1234 te permite ingresar a la parte de tablas y eliminar alumnos o cambiar datos REQUIERE INGRESAR PhpMyAdmin ",
+    link: "https://github.com/Gonzalo67diaz/back-PhpMyAdmin-PHP-Y-SQL/tree/main",
+    imagen: "sistem.png",
+    tecnologias: ["PhpMyAdmin", "PHP", "SQL"],
+  },
+
+  {
+    titulo: "PROYECTO-AGENDA LOCALSTORAGEs ",
+    descripcion:
+      "hola este es mi proyeto de agenda donde podes agendar tu tareas en un textarea con un limite de letras lo mejor del proyecto es que lo que agendas queda guardado en el localstorage si recargas la pagina la informacion se mantiene y asi podes ir agregando tareas o eliminar",
+    link: "https://6530169969983b3014e581a8--harmonious-frangollo-d1cd43.netlify.app/",
+    imagen: "agenda.png",
+    tecnologias: ["HTML", "CSS", "Javascript"],
+  },
+
+
+
+];
+
+const informacionPersonal = {
+  nombre: "Gonzalo Diaz",
+  subtitulo: "Desarrollador web",
+  imagen: "img/gonza1-removebg-preview.png",
+  otros: [
+    ["Nacionalidad", "Argentino"],
+    [
+      "Edad",
+      new Date(new Date() - new Date("1995/08/05")).getFullYear()-1970 + " años",
+    ],
+  ],
+  idiomas: [
+    ["Español", "Nativo"],["Inglés", "Intermedio"],
+  ],
+
+  Estudios: [
+    ["Estudios","UTN Córdoba Sistemas"],
+  ],
+
+  tecnologias: [
+    ["Html", 7],["Css", 7],["Javascript", 7],["Typescript", 7],["React", 6.60
+    ],["PHP",7],["Node.js",6],["SQL",6],["Consumo de Api ",6.60],[".JSON",6.60],
+  ],
+  redes: [
+
+    ["github", "https://github.com/Gonzalo67diaz"],
+    ["linkedin", "https://www.linkedin.com/in/gonzalo-diaz-790649283/"],
+  
+  ]
+};
